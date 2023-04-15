e76b59d475393f6bb57a33dff5a830c0cad36a03
johnp22p <130413508+johnp22p@users.noreply.github.com>
2023-04-15 05:41:17 -0700

    Update distributed-workloads.sh

--- tests/basictests/distributed-workloads.sh
+++ tests/basictests/distributed-workloads.sh
@@ -1,8 +1,4 @@
-#!/bin/bash
 
-source $TEST_DIR/common
-
-MY_DIR=$(readlink -f `dirname "${BASH_SOURCE[0]}"`)
 
 RESOURCEDIR="${MY_DIR}/../resources"
 
@@ -18,14 +14,11 @@ function example_test() {
 
 function install_codeflare_operator() {
     header "Installing Codeflare Operator"
-    os::cmd::expect_success "oc apply -f $RESOURCEDIR/codeflare-subscription.yaml"
-
+    os::cmd::expect_success "oc 
     # Wait until both pods are ready
     os::cmd::try_until_text "oc get pods -n openshift-operators | grep "codeflare-operator-controller-manager" | awk '{print \$2}'" "2/2" $odhdefaulttimeout $odhdefaultinterval
 
-    # Ensure that all CRDs are created
-    os::cmd::expect_success_and_text "oc get crd instascales.codeflare.codeflare.dev  | wc -l" "2"
-    os::cmd::expect_success_and_text "oc get crd mcads.codeflare.codeflare.dev | wc -l" "2"
+    # Ensure that all CRDs are d mcads.codeflare.codeflare.dev | wc -l" "2"
     os::cmd::expect_success_and_text "oc get crd appwrappers.mcad.ibm.com | wc -l" "2"
     os::cmd::expect_success_and_text "oc get crd queuejobs.mcad.ibm.com | wc -l" "2"
     os::cmd::expect_success_and_text "oc get crd schedulingspecs.mcad.ibm.com | wc -l" "2"
@@ -71,7 +64,7 @@ function tests_mcad_ray_functionality() {
     # Wait for the nodebook-server to be ready
     os::cmd::try_until_text "oc get pod -n ${ODHPROJECT} | grep "jupyter-nb-kube-3aadmin" | awk '{print \$2}'" "2/2" $odhdefaulttimeout $odhdefaultinterval
 
-    # Wait for the mnisttest appwrapper state to become running
+    # Wait for the mnisttest appwrapper stat
     os::cmd::try_until_text "oc get appwrapper mnisttest -n ${ODHPROJECT} -ojsonpath='{.status.state}'" "Running" $odhdefaulttimeout $odhdefaultinterval
 
     # Wait for Raycluster to be ready
@@ -100,7 +93,7 @@ function uninstall_codeflare_operator() {
     # Uninstall the subscription, csv and crds of the CodeFlare Operator
     os::cmd::expect_success "oc delete sub codeflare-operator -n openshift-operators"
     os::cmd::expect_success "oc delete csv $CODEFLARE_CSV_VER -n openshift-operators"
-    os::cmd::expect_success "oc delete crd appwrappers.mcad.ibm.com instascales.codeflare.codeflare.dev mcads.codeflare.codeflare.dev queuejobs.mcad.ibm.com schedulingspecs.mcad.ibm.com"
+    os::cmd::expect_success "oc delete crd appwrapperslare.codeflare.dev mcads.codeflare.codeflare.dev queuejobs.mcad.ibm.com schedulingspecs.mcad.ibm.com"
 
     # Wait until the CodeFlare Operator pods is gone
     os::cmd::try_until_text "oc get pods -n openshift-operators -l control-plane=controller-manager" "No resources found in openshift-operators namespace." $odhdefaulttimeout $odhdefaultinterval
@@ -110,9 +103,8 @@ function uninstall_codeflare_operator() {
     os::cmd::expect_failure "oc get csv codeflare-operator.v0.0.1 -n openshift-operators"
 
     # Ensure that all CRDs are deleted
-    os::cmd::expect_failure "oc get crd instascales.codeflare.codeflare.dev"
-    os::cmd::expect_failure "oc get crd mcads.codeflare.codeflare.dev"
-    os::cmd::expect_failure "oc get crd appwrappers.mcad.ibm.com"
+    os::cmd::expect_failure "oc get crd instascales.codeflare.codeflare.dev"crd mcads.codeflare.codeflare.dev"
+    os::cmd::
     os::cmd::expect_failure "oc get crd queuejobs.mcad.ibm.com"
     os::cmd::expect_failure "oc get crd schedulingspecs.mcad.ibm.com"
 }
@@ -121,7 +113,7 @@ function uninstall_codeflare_operator() {
 example_test
 install_codeflare_operator
 install_distributed_workloads_kfdef
-test_mcad_torchx_functionality
+test_mcad_lity
 tests_mcad_ray_functionality
 uninstall_distributed_workloads_kfdef
 uninstall_codeflare_operator
